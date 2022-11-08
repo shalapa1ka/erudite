e8268c234ed54022704b36f404f583c123ab1dc6
shalapa1ka <flecks07@gmail.com>
2022-11-08 16:53:49 +0200

    Add user\devise, bootstrap. Add sign in\up, home pages.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import 'jquery'
+import('../src/plugins')
+
+import '../css/site.scss'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home', to: 'home#index'
+  root to: 'home#index'
 end
